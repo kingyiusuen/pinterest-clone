4d037344bf70ce1eb91776f7ee609a7f7859ee6f
King Yiu Suen <kingyiusuen@gmail.com>
2021-11-12 19:32:42 -0600

    Modify code for production

--- server/app.js
+++ server/app.js
@@ -20,13 +20,22 @@ mongoose
   });
 
 app.use(cors());
-app.use(express.static("build"));
 app.use(express.json());
+
 app.use(passport.initialize());
 require("./utils/passport")(passport);
+
 app.use(middleware.requestLogger);
 
 app.use("/api/users", usersRouter);
+
+if (process.env.NODE_ENV === "production") {
+  const buildPath = path.resolve(__dirname, "..", "client", "build");
+  app.use(express.static(buildPath));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(buildPath, "index.html"));
+  })
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI;
 
 module.exports = {
